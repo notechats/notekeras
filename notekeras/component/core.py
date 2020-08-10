587478bedef1ffd4d6859aef2825ef5e4714b801
niuliangtao@youzan.com <niuliangtao@youzan.com>
2020-08-10 16:50:03 +0800

    add

--- notekeras/component/core.py
+++ notekeras/component/core.py
@@ -1,6 +1,30 @@
 import numpy as np
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Layer
 from tensorflow.keras.models import Model
+
+
+class ComponentLayer(Layer):
+    def __init__(self, inputs=None, layer_depth=1, *args, **kwargs):
+        """
+        :param layer_depth:
+        :param as_model:组件是否当做一个模型返回，当模型的时候，输入inputs必须非空
+        :param inputs:模型输入，当as_model为True时传入
+        :param args:
+        :param kwargs:
+        """
+        super(ComponentLayer, self).__init__(*args, **kwargs)
+
+        self.layer_depth = layer_depth
+        self.input_layer = inputs
+
+        super(ComponentLayer, self).__init__(*args, **kwargs)
+
+    def __call__(self, inputs, **kwargs):
+        if self.layer_depth <= 0:
+            return super(ComponentLayer, self).__call__(inputs=inputs, **kwargs)
+        else:
+            self.build(np.shape(inputs)[1:])
+            return self.call(inputs, **kwargs)
 
 
 class Component(Model):
@@ -38,7 +62,7 @@ class Component(Model):
             self.build(np.shape(inputs)[1:])
             return self.call(inputs, **kwargs)
 
-    
+
 class AutoEncoder(Component):
     def __init__(self, name='AutoEncoder', encode_size=None, *args, **kwargs):
         self.encode_size = encode_size or [128, 64, 32]
--- notekeras/component/yolo/yolo3.py
+++ notekeras/component/yolo/yolo3.py
@@ -23,40 +23,23 @@ class DarkConv(Component):
         self.count = count
         self.size = size
         self.strides = strides
-        self.conv = self.norm = self.relu = None
+        self.compose_list = None
         super(DarkConv, self).__init__(*args, **kwargs)
 
     def build(self, input_shape):
-        self.conv = DarknetConv2D(self.count,
-                                  self.size,
-                                  use_bias=False,
-                                  name='{}-conv'.format(self.name),
-                                  strides=self.strides)
-        self.norm = BatchNormalization(name='{}-norm'.format(self.name))
-
-        self.relu = LeakyReLU(alpha=0.1, name='{}-relu'.format(self.name))
+        self.compose_list = [
+            DarknetConv2D(self.count, self.size, use_bias=False, name='{}-conv'.format(self.name),
+                          strides=self.strides),
+            BatchNormalization(name='{}-norm'.format(self.name)),
+            LeakyReLU(alpha=0.1, name='{}-relu'.format(self.name))
+        ]
 
     def call(self, inputs, **kwargs):
-        # output = compose(self.conv, self.norm, self.relu)(inputs)
-        output = self.conv(inputs)
-        output = self.norm(output)
-        output = self.relu(output)
+        output = compose(*self.compose_list)(inputs)
         return output
 
     def compute_output_shape(self, input_shape):
         return self.conv.compute_output_shape(input_shape)
-
-    def _build(self, inputs):
-        output = DarknetConv2D(self.count,
-                               self.size,
-                               use_bias=False,
-                               name='{}-conv'.format(self.name),
-                               strides=self.strides)(inputs)
-        output = BatchNormalization(name='{}-norm'.format(self.name))(output)
-
-        output = LeakyReLU(alpha=0.1, name='{}-relu'.format(self.name))(output)
-
-        return output
 
 
 class DarkConvSet(Component):
@@ -76,17 +59,6 @@ class DarkConvSet(Component):
 
     def call(self, inputs, **kwargs):
         return compose(*self.conv_list)(inputs)
-
-    def _build(self, inputs):
-        res = compose(
-            DarkConv(self.num_filters, (1, 1), name='{}_1'.format(self.name)),
-            DarkConv(self.num_filters * 2, (3, 3), name='{}_2'.format(self.name)),
-            DarkConv(self.num_filters, (1, 1), name='{}_3'.format(self.name)),
-            DarkConv(self.num_filters * 2, (3, 3), name='{}_4'.format(self.name)),
-            DarkConv(self.num_filters, (1, 1), name='{}_5'.format(self.name))
-        )(inputs)
-
-        return res
 
 
 class ResBlockBody(Component):
@@ -113,20 +85,9 @@ class ResBlockBody(Component):
 
     def call(self, inputs, **kwargs):
         x = compose(self.zero, self.conv)(inputs)
-        for block in self.block_layers:
-            conv1, conv2, add = block
+        for conv1, conv2, add in self.block_layers:
             y = compose(conv1, conv2)(x)
             x = add([x, y])
-        return x
-
-    def _build(self, inputs):
-        x = ZeroPadding2D(((1, 0), (1, 0)))(inputs)
-
-        x = DarkConv(self.num_filters, (3, 3), strides=(2, 2), layer_depth=self.layer_depth - 1)(x)
-        for i in range(self.num_blocks):
-            y = DarkConv(self.num_filters // 2, (1, 1), layer_depth=self.layer_depth - 1)(x)
-            y = DarkConv(self.num_filters, (3, 3), layer_depth=self.layer_depth - 1)(y)
-            x = Add()([x, y])
         return x
 
 
@@ -137,7 +98,7 @@ class YoloModel(Component):
         self.anchors = anchors
 
         self.layer0 = self.block1 = self.block2 = self.block3 = self.block4 = self.block5 = None
-        self.make_list = self.samps = self.concat = None
+        self.make_list = self.conv_samp = self.concat = None
         super(YoloModel, self).__init__(name=name, *args, **kwargs)
 
     def build(self, input_shape):
@@ -156,7 +117,7 @@ class YoloModel(Component):
              DarknetConv2D(out_filters, (1, 1), name='yolo_conv2d_{}'.format(num_filters))
              ] for num_filters in [512, 256, 128]
         ]
-        self.samps = [[
+        self.conv_samp = [[
             DarkConv(256, (1, 1), layer_depth=self.layer_depth - 2, name='{}__DarkConv_1'.format(self.name), ),
             UpSampling2D(2)
         ], [
@@ -177,60 +138,16 @@ class YoloModel(Component):
         x = self.make_list[0][0](out5)
         y1 = compose(*self.make_list[0][1:])(x)
 
-        x = compose(*self.samps[0])(x)
+        x = compose(*self.conv_samp[0])(x)
         x = self.concat[0]([x, out4])
 
         x = self.make_list[1][0](x)
         y2 = compose(*self.make_list[1][1:])(x)
 
-        x = compose(*self.samps[1])(x)
+        x = compose(*self.conv_samp[1])(x)
         x = self.concat[1]([x, out3])
 
         x = self.make_list[2][0](x)
         y3 = compose(*self.make_list[2][1:])(x)
 
         return y1, y2, y3
-
-    # def _build(self, inputs):
-    #     layer0 = DarkConv(32, (3, 3), name='{}_DarkConv'.format(self.name),
-    #                       layer_depth=self.layer_depth - 2)(inputs)
-    #     block1 = ResBlockBody(64, 1, name='{}_ResBlock_1'.format(self.name),
-    #                           layer_depth=self.layer_depth - 1)(layer0)
-    #     block2 = ResBlockBody(128, 2, name='{}_ResBlock_2'.format(self.name),
-    #                           layer_depth=self.layer_depth - 1)(block1)
-    #     block3 = ResBlockBody(256, 8, name='{}_ResBlock_3'.format(self.name),
-    #                           layer_depth=self.layer_depth - 1)(block2)
-    #     block4 = ResBlockBody(512, 8, name='{}_ResBlock_4'.format(self.name),
-    #                           layer_depth=self.layer_depth - 1)(block3)
-    #     block5 = ResBlockBody(1024, 4, name='{}_ResBlock_5'.format(self.name),
-    #                           layer_depth=self.layer_depth - 1)(block4)
-    #
-    #     x, y1 = self.make_last_layers(block5, 512)
-    #
-    #     x = compose(DarkConv(256, (1, 1), layer_depth=self.layer_depth - 2, name='{}__DarkConv_2'.format(self.name), ),
-    #                 UpSampling2D(2))(x)
-    #
-    #     x = Concatenate()([x, block4])
-    #     x, y2 = self.make_last_layers(x, 256)
-    #
-    #     x = compose(DarkConv(128, (1, 1), layer_depth=self.layer_depth - 2, name='{}__DarkConv_3'.format(self.name), ),
-    #                 UpSampling2D(2))(x)
-    #     x = Concatenate()([x, block3])
-    #     x, y3 = self.make_last_layers(x, 128)
-    #
-    #     return [y1, y2, y3]
-    #
-    # def make_last_layers(self, inputs, num_filters, out_filters=None):
-    #     out_filters = out_filters or self.num_anchors * (self.num_classes + 5)
-    #
-    #     x = DarkConvSet(num_filters, layer_depth=self.layer_depth - 1)(inputs)
-    #
-    #     print(x)
-    #     y = DarkConv(num_filters * 2, (3, 3), layer_depth=self.layer_depth - 1)(x)
-    #     y = DarknetConv2D(out_filters, (1, 1), name='yolo_conv2d_{}'.format(num_filters))(y)
-    #
-    #     # y = compose(
-    #     #     DarkConv(num_filters * 2, (3, 3), layer_depth=self.layer_depth - 1),
-    #     #     DarknetConv2D(out_filters, (1, 1), name='yolo_conv2d_{}'.format(num_filters))
-    #     # )(x)
-    #     return x, y
