d9194d500ca57acdcdfc102aee954cfbaaef31dd
niuliangtao <niuliangtao@qq.com>
2020-10-19 14:22:59 +0800

    add

--- notekeras/layer/fm.py
+++ notekeras/layer/fm.py
@@ -5,20 +5,25 @@ from tensorflow.keras.regularizers import l2
 
 
 class FactorizationMachine(Layer):
-    """
-    """
 
     def __init__(self,
-                 output_dim=30,
+                 output_dim=1,
                  factor_dim=10,
                  name='FM',
                  # activation="relu",
                  activation=None,
                  use_weight=True,
                  use_bias=True,
+                 kernal_reg=1e-4,
+                 weight_reg=1e-4,
                  *args,
                  **kwargs):
         """
+        Factorization Machines
+        :param output_dim: 输出维度
+        :param factor_dim: 隐含向量维度
+        :param w_reg: the regularization coefficient of parameter w
+        :param v_reg: the regularization coefficient of parameter v
         """
         super(FactorizationMachine, self).__init__(name=name, *args, **kwargs)
         self.output_dim = output_dim
@@ -26,6 +31,8 @@ class FactorizationMachine(Layer):
         self.activate = activation
         self.user_weight = use_weight
         self.use_bias = use_bias
+        self.kernal_reg = kernal_reg
+        self.weight_reg = weight_reg
         self.weight = self.bias = self.kernel = None
         self.activate_layer = None
 
@@ -33,14 +40,14 @@ class FactorizationMachine(Layer):
         self.kernel = self.add_weight(name='kernel',
                                       shape=(input_shape[1], self.factor_dim),
                                       initializer='glorot_uniform',
-                                      regularizer=l2(1e-4),
+                                      regularizer=l2(self.kernal_reg),
                                       trainable=True)
         if self.user_weight:
             self.weight = self.add_weight(name='weight',
                                           shape=(
                                               input_shape[1], self.output_dim),
                                           initializer='glorot_uniform',
-                                          regularizer=l2(1e-4),
+                                          regularizer=l2(self.weight_reg),
                                           trainable=True)
         if self.use_bias:
             self.bias = self.add_weight(name='bias',
@@ -76,58 +83,3 @@ class FactorizationMachine(Layer):
 
 
 FM = FactorizationMachine
-
-
-class FMLayer(Layer):
-    def __init__(self, output_dim,
-                 factor_order,
-                 activation=None,
-                 **kwargs):
-        if 'input_shape' not in kwargs and 'input_dim' in kwargs:
-            kwargs['input_shape'] = (kwargs.pop('input_dim'),)
-        super(FMLayer, self).__init__(**kwargs)
-
-        self.output_dim = output_dim
-        self.factor_order = factor_order
-        self.activation = keras.activations.get(activation)
-
-    def build(self, input_shape):
-        assert len(input_shape) == 2
-        input_dim = input_shape[1]
-
-        self.w = self.add_weight(name='one',
-                                 shape=(input_dim, self.output_dim),
-                                 initializer='glorot_uniform',
-                                 trainable=True)
-        self.v = self.add_weight(name='two',
-                                 shape=(input_dim, self.factor_order),
-                                 initializer='glorot_uniform',
-                                 trainable=True)
-        self.b = self.add_weight(name='bias',
-                                 shape=(self.output_dim,),
-                                 initializer='zeros',
-                                 trainable=True)
-
-        super(FMLayer, self).build(input_shape)
-
-    def call(self, inputs, **kwargs):
-        X_square = K.square(inputs)
-
-        xv = K.square(K.dot(inputs, self.v))
-        xw = K.dot(inputs, self.w)
-
-        p = 0.5 * K.sum(xv - K.dot(X_square, K.square(self.v)), 1)
-        rp = K.repeat_elements(K.reshape(p, (-1, 1)), self.output_dim, axis=-1)
-
-        f = xw + rp + self.b
-
-        output = K.reshape(f, (-1, self.output_dim))
-
-        if self.activation is not None:
-            output = self.activation(output)
-
-        return output
-
-    def compute_output_shape(self, input_shape):
-        assert input_shape and len(input_shape) == 2
-        return input_shape[0], self.output_dim
