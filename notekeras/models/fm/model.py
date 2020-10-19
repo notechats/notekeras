93c033f3fb82eb3ea107605ef9059b77d6beba49
niuliangtao <niuliangtao@qq.com>
2020-10-19 20:49:42 +0800

    add

--- notekeras/models/fm/model.py
+++ notekeras/models/fm/model.py
@@ -354,7 +354,8 @@ class xDeepFM(keras.Model):
         }
         self.linear = Linear()
         self.cin = CIN(cin_size=cin_size, l2_reg=cin_reg)
-        self.dnn = DNN(hidden_units=hidden_units, dropout == dnn_dropout, activation == dnn_activation)
+        self.dnn = DNN(hidden_units=hidden_units,
+                       dropout=dnn_dropout, activation=dnn_activation)
         self.cin_dense = Dense(1)
         self.dnn_dense = Dense(1)
         self.bias = self.add_weight(name='bias', shape=(
