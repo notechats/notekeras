c2ea37fdde82710c628be6f5510e2963e9bba798
niuliangtao <niuliangtao@qq.com>
2020-10-24 12:13:14 +0800

    add

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 import sys
 from os import path
 
-from setuptools import setup, find_packages
-
 from notebuild.tool import get_version
+from setuptools import find_packages, setup
 
-version_path = path.join(path.abspath(path.dirname(__file__)), 'script/__version__.md')
+version_path = path.join(path.abspath(
+    path.dirname(__file__)), 'script/__version__.md')
 
-version = get_version(sys.argv, version_path, step=16)
+version = get_version(sys.argv, version_path, step=32)
 
-install_requires = ['tensorflow', 'numpy', 'scipy', 'opencv-python', 'pillow', 'tqdm']
+install_requires = ['tensorflow', 'numpy',
+                    'scipy', 'opencv-python', 'pillow', 'tqdm']
 
 setup(name='notekeras',
       version=version,
