6c2b34fb13a9c3fa61ad17918f5ab6d8b6bd42ae
niuliangtao <niuliangtao@qq.com>
2020-10-29 15:26:15 +0800

    add

--- notekeras/models/fm/core.py
+++ notekeras/models/fm/core.py
@@ -5,13 +5,22 @@ from tensorflow.keras.layers import Dense
 
 
 class DeepFM(Component):
-    def __init__(self, k=10, hidden_units=(200, 200, 200), dnn_dropout=0.,
-                 activation='relu', fm_w_reg=1e-4, fm_v_reg=1e-4, *args, **kwargs):
+    def __init__(self,
+                 output_dim=1,
+                 activation='sigmoid',
+                 dnn_units=(200, 200, 200),
+                 dnn_dropout=0.,
+                 dnn_activation='relu',
+                 fm_factor_dim=10,
+                 fm_output_dim=1,
+                 fm_w_reg=1e-4,
+                 fm_v_reg=1e-4,
+                 *args, **kwargs):
         """
         DeepFM
         :param feature_columns: A list. a list containing dense and sparse column feature information.
-        :param k: A scalar. fm's latent vector number.
-        :param hidden_units: A list. A list of dnn hidden units.
+        :param fm_factor_dim: A scalar. fm's latent vector number.
+        :param dnn_units: A list. A list of dnn hidden units.
         :param dnn_dropout: A scalar. Dropout of dnn.
         :param activation: A string. Activation function of dnn.
         :param fm_w_reg: A scalar. The regularizer of w in fm.
@@ -19,20 +28,29 @@ class DeepFM(Component):
         :param embed_reg: A scalar. The regularizer of embedding.
         """
         super(DeepFM, self).__init__(*args, **kwargs)
-        self.factor_dim = k
-        self.hidden_units = hidden_units
+        self.output_dim = output_dim
         self.activation = activation
+
+        self.dnn_units = dnn_units
         self.dnn_dropout = dnn_dropout
+        self.dnn_activation = dnn_activation
+
         self.fm_v_reg = fm_v_reg
         self.fm_w_reg = fm_w_reg
+        self.fm_factor_dim = fm_factor_dim
+        self.fm_output_dim = fm_output_dim
+
         self.fm = self.dense = self.w1 = self.w2 = self.bias = self.dnn = None
 
     def build(self, input_shape):
-        self.fm = FactorizationMachine(output_dim=1, factor_dim=self.factor_dim, kernal_reg=self.fm_v_reg,
-                                       weight_reg=self.fm_w_reg, name='FM')
+        self.fm = FactorizationMachine(output_dim=self.fm_output_dim,
+                                       factor_dim=self.fm_factor_dim,
+                                       kernal_reg=self.fm_v_reg,
+                                       weight_reg=self.fm_w_reg,
+                                       name='FM')
 
-        self.dnn = DNN(self.hidden_units, self.activation, self.dnn_dropout)
-        self.dense = Dense(1, activation='sigmoid')
+        self.dnn = DNN(self.dnn_units, self.dnn_activation, self.dnn_dropout)
+        self.dense = Dense(self.output_dim, activation=self.activation)
 
     def call(self, inputs, **kwargs):
         wide_outputs = self.fm(inputs)
