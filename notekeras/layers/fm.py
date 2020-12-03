50d605def92dbab46d79e68faf9018906d892d0f
niuliangtao <niuliangtao@qq.com>
2020-12-03 19:54:04 +0800

    add

--- notekeras/layers/fm.py
+++ notekeras/layers/fm.py
@@ -1,8 +1,7 @@
 import tensorflow as tf
-from tensorflow import keras
 from tensorflow.keras import activations
 from tensorflow.keras import backend as K
-from tensorflow.keras.layers import  Layer
+from tensorflow.keras.layers import Layer
 from tensorflow.keras.regularizers import l2
 
 
@@ -12,11 +11,10 @@ class FactorizationMachine(Layer):
                  output_dim=1,
                  factor_dim=10,
                  name='FM',
-                 # activation="relu",
                  activation=None,
                  use_weight=True,
                  use_bias=True,
-                 kernal_reg=1e-4,
+                 kernel_reg=1e-4,
                  weight_reg=1e-4,
                  *args,
                  **kwargs):
@@ -31,9 +29,9 @@ class FactorizationMachine(Layer):
         self.output_dim = output_dim
         self.factor_dim = factor_dim
         self.activate = activation
-        self.user_weight = use_weight
+        self.use_weight = use_weight
         self.use_bias = use_bias
-        self.kernal_reg = kernal_reg
+        self.kernel_reg = kernel_reg
         self.weight_reg = weight_reg
         self.weight = self.bias = self.kernel = None
         self.activate_layer = None
@@ -42,9 +40,9 @@ class FactorizationMachine(Layer):
         self.kernel = self.add_weight(name='kernel',
                                       shape=(input_shape[1], self.factor_dim),
                                       initializer='glorot_uniform',
-                                      regularizer=l2(self.kernal_reg),
+                                      regularizer=l2(self.kernel_reg),
                                       trainable=True)
-        if self.user_weight:
+        if self.use_weight:
             self.weight = self.add_weight(name='weight',
                                           shape=(
                                               input_shape[1], self.output_dim),
@@ -57,7 +55,7 @@ class FactorizationMachine(Layer):
                                         initializer='zeros',
                                         trainable=True)
 
-        self.activate_layer = keras.activations.get(self.activate)
+        self.activate_layer = activations.get(self.activate)
         super(FactorizationMachine, self).build(input_shape)
 
     def call(self, inputs, **kwargs):
@@ -67,7 +65,7 @@ class FactorizationMachine(Layer):
         xv = K.repeat_elements(K.reshape(p, (-1, 1)), self.output_dim, axis=-1)
 
         res = xv
-        if self.user_weight:
+        if self.use_weight:
             res = res + K.dot(inputs, self.weight)
         if self.use_bias:
             res = res + self.bias
@@ -82,6 +80,20 @@ class FactorizationMachine(Layer):
     def compute_output_shape(self, input_shape):
         assert input_shape and len(input_shape) == 2
         return input_shape[0], self.output_dim
+
+    def get_config(self):
+        config = {
+            "output_dim": self.output_dim,
+            "factor_dim": self.factor_dim,
+            "name": self.name,
+            "kernel_reg": self.kernel_reg,
+            "weight_reg": self.weight_reg,
+            "use_weight": self.use_weight,
+            "use_bias": self.use_bias,
+            "activation": self.activate
+        }
+        config.update(super(FactorizationMachine, self).get_config())
+        return config
 
 
 FM = FactorizationMachine
@@ -151,3 +163,17 @@ class FFM(Layer):
             output = self.activate_layer(output)
 
         return output
+
+    def get_config(self):
+        config = {
+            "name": self.name,
+            "field_num": self.field_num,
+            "factor_dim": self.factor_dim,
+            "use_weight": self.use_weight,
+            "use_bias": self.use_bias,
+            "weight_reg": self.weight_reg,
+            "kernel_reg": self.kernel_reg,
+            "activation": self.activation
+        }
+        config.update(super(FFM, self).get_config())
+        return config
